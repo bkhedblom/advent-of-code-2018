1a5e36278817e5f5c0f7bb62caf2c2f8155d8e2b
Björn Hedblom <git@singali.net>
2018-12-02 03:18:46 +0100

    Adds some style

--- src/app/solver-base.ts
+++ src/app/solver-base.ts
@@ -3,15 +3,27 @@ import { Input, OnInit } from "@angular/core";
 export abstract class SolverBase implements OnInit {
     @Input() input:string;
     solution:number;
+    showInput = false;
+    get shortInput() { return this.input.substr(0,10)}
+
     constructor() { }
-  
+    get name(){
+         const name = this.constructor.name;
+         const length = name.length;
+         const lengthToRemove = "Component".length;
+         return name.substr(0,length-lengthToRemove);
+    }
     ngOnInit() {
       this.solution = this.solve(this.input);
     }
 
     protected abstract solve(input:string): number;
-    
+
     protected splitToNumberArray(toSplit:string, delimiter = "\n"){
         return toSplit.split(delimiter).map((value) => Number(value))
+    }
+
+    toggleInput = () => {
+        this.showInput = !this.showInput;
     }
 }
