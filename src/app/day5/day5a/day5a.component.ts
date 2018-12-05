a070b291687750b1d788f4cf1bfde8810b27c728
Björn Hedblom <git@singali.net>
2018-12-06 00:47:43 +0100

    Faster day5a solution with RegExp

--- src/app/day5/day5a/day5a.component.ts
+++ src/app/day5/day5a/day5a.component.ts
@@ -7,27 +7,25 @@ import { SolverBase } from 'src/app/solver-base';
   styleUrls: ['./day5a.component.css']
 })
 export class Day5aComponent extends SolverBase{
-  protected solve(input: string[]): string | number {
-    input = this.input.split("");
-    for (let i = 0; i < input.length - 1; i++) {
-      const current = input[i];
-      if(!current) continue;
-      let next:string;
-      let k = 0;
-      while(!next){
-        k++;
-        next = input[i + k];
-      } 
-      if (current != next && current.toLowerCase() == next.toLowerCase()) {
-        input[i] = input[i+k] = undefined;
-        if (i < 1) {
-          i = -1
-        } else {
-          i =- 2;
-        }
-      }
-    }
-    return input.filter((x) => x).length;
+  protected solve(input: string[]): number {
+    return this.regexSolve(this.input);
+  }
+  regexSolve(input:string): number {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    let regex = "(" + 
+                alphabet.map(
+                  (letter) => `(${letter}${letter.toUpperCase()}|${letter.toUpperCase()}${letter})`
+                )
+                .join("|")
+              + ")";
+    const reducable = new RegExp(regex, "g");
+    let oldLen:number;
+    do {
+      oldLen = input.length;
+      input = input.replace(reducable, "");
+    } while (oldLen != input.length);
+
+    return input.length;
   }
 
 }
